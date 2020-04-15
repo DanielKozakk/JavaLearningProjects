5ecd9b47b0fad6ad3f823296cf7b0c6464603979
DanielKozakk <kozak2321@gmail.com>
2020-04-15 10:49:35 +0200

    initial comit

---
+++ helloWorld/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("19 minut pozostało - zrób ćwiczenia w exercism");
+
+
+    }
+}
---
+++ struktury_danych/src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // PRZYKŁADOWE STRUKTURY DANYCH:
+        // array - czyli struktura danych, każda wartość ma przypisany liczboy klucz:
+        // key - 0, value - żółty
+        // key - 1, value - czerwony
+        // key - 2, value - biały
+
+
+        // QUEUE - kolejka
+        // dodawanie, usuwanie, pobieranie w takiej kolejności, w jakiej je dodaliśmy
+        // jest to FIFO - first in, first out
+
+        // STACK - stos
+        //  jest to LIFO - Last In, First Out, operacje pobierania danych to zdejmowanie elementów ze stosu
+
+        // TREE - drzewo
+        // każdy element może posiadać rodziców oraz dzieci
+
+        //NAJPOPULARNIEJSZE STRUKTURY DANYCH:
+        // ArrayList - posiada więcej możliwości niż Array
+
+
+    }
+}
