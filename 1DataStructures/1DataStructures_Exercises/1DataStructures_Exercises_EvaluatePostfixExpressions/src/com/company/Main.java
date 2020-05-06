3c72bf0bb090e9bece13db8cffc69fb58af1f301
DanielKozakk <kozak2321@gmail.com>
2020-05-06 14:28:23 +0200

    started to write function for postfix evaluation

---
+++ 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_EvaluatePostfixExpressions/src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public int evaluatePostFixExpression(String expressionToEvaluate) {
+
+        int lengthOfExpression = expressionToEvaluate.length();
+
+        Stack<String> stackExpression = new Stack();
+
+        List<String> list = Arrays.asList(new String[]{"+", "-", "*", "/"});
+
+
+        for (int i = 0; i < lengthOfExpression; i++) {
+
+
+            if()
+
+
+            expressionToEvaluate.charAt(i)
+        }
+
+        return 2;
+    }
+}
