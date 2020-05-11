3e6a4f0be1c3c686e03cdc3e03241188f88b52c0
DanielKozakk <kozak2321@gmail.com>
2020-05-11 13:50:19 +0200

    collection2 second try

--- 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_General_2/src/com/company/Main.java
+++ 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_General_2/src/com/company/Main.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class Main {
 
-    public static int[] firstExampleIntUnsortedArray = {-1, 1, 4, 1, 4, 1, 4, 3, 3, 4, 4, 2, 2, -10};
+    public static int[] firstExampleIntUnsortedArray = { 1, 4, 1, 4, 1, 4, 3, 3, 4, 4, 2, 2, -10};
 
     public static int[] secondExampleIntSortedArray = {1, 5, 8, 20, 21, 26, 29, 31, 46, 79, 80, 101, 242, 333};
     public static int[] thirdExampleIntSortedArray = {-121, -120, -40, -31, -9, 0, 5, 10, 15, 37, 40, 80, 81, 82};
@@ -11,10 +11,105 @@ public class Main {
     public static String[] sixthExampleStringArray = {"beb", "Your", "leg", "is", "very", "sexy!", "very", "a", "a", "a"};
 
     public static int[] seventhExampleIntSortedArray = {1, 3, 4, 5, 7};
-    public static int[] eighthExampleIntSortedArray = {2, 3, 5, 6} ;
+    public static int[] eighthExampleIntSortedArray = {2, 3, 5, 6};
+
 
 
     public static void main(String[] args) {
-	// write your code here
+
+//        System.out.println(collections_1_findSecondMinimumElement(thirdExampleIntSortedArray));
+        System.out.println(collections_2_firstUniqueElement(firstExampleIntUnsortedArray));
+
     }
+
+
+    public static int collections_2_firstUniqueElement(int[] inputArray) {
+        int arrayLength = inputArray.length;
+        /*
+
+        NA PAPIERZE:
+
+        Mam daną tablicę która posiada zduplikowane elementy
+        tworzę zmienną z elmeentme unikalnym
+        Iteruje się po nich w jednej pętli z zmienną i
+        Potem w środku robię drugą pętlę z zmienną j
+
+        uniqueElement=inputArr[j]
+
+        W środkowej pętli sprawdzam czy takie same są inputArray[i] i inputArray[j] oraz czy indeksy i oraz j nie są identyczne
+        jeśli tak, to uniqueElement = null.
+
+
+        W wewnętrznej pętli tworzę warunek, który sprawdza czy uniqueElement != null, jeśi tak, przerywa działanie pętli
+        Zwraca Unique
+
+        PSEUDOKOD:
+
+        int [] inputArr;
+        uniqueElement= null;
+        for loop z i po inputArray
+            for loop z j po inputArray
+                  uniqueElemenet=inputArr[j]
+                    jeśli element inputArr[j] jest taki sam jak inputArr[i] oraz indeksy nie są tożsame to
+                        uniqueElement = null;
+            jeśli uniqueElemenet !=null
+             to break
+
+          return uniqueElement
+        */
+
+        Integer uniqueElement = null;
+        for(int i = 0; i < arrayLength; i++){
+            uniqueElement = inputArray[i];
+            for(int j = 0; j < arrayLength; j++){
+                if(i != j && inputArray[i] == inputArray [j]){
+                    uniqueElement = null;
+                }
+            }
+            if(uniqueElement != null){
+                break;
+            }
+        }
+        return uniqueElement;
+
+    }
+
+    public static int collections_1_findSecondMinimumElement(int[] inputArray) {
+
+        /*
+            NA PAPIERZE:
+            robię dwie zmienne, jedna na firstMinimum, druga secon Miniumm
+            iteruje się przez tablicę
+            sprawdzam, czy element jest mniejszy niż firstMinium, jeśli tak to secondMinnmum = firstMinimum, firstMinimum = element
+            else if sprawdzam czy element jest mniejszy niż second minimum, jeśli tak to secondMinium = element
+
+            PSEUDOKOD:
+
+            INT SECOND_MINIMUM;
+            INT FIRST_MNIMUM;
+
+            FOR(INT ELEMENT : ARRAY){
+
+                    IF ( ELEMENT < FIRST MINIMUM)
+
+                            ELSE IF (ELEMENT < SECOND MINIMUM
+            }
+         */
+
+        int firstMinimum = Integer.MAX_VALUE;
+        int secondMinimum = Integer.MAX_VALUE;
+
+        for (int element : inputArray) {
+            if (element < firstMinimum) {
+                secondMinimum = firstMinimum;
+                firstMinimum = element;
+            } else if (element < secondMinimum) {
+
+                secondMinimum = element;
+            }
+        }
+
+        return secondMinimum;
+    }
+
 }
