3879a30fcfce5c36cc3d543f9679b14518be5ac9
DanielKozakk <kozak2321@gmail.com>
2020-05-11 10:19:18 +0200

    challenges season 2

---
+++ 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_General_2/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static int[] firstExampleIntUnsortedArray = {-1, 1, 4, 1, 4, 1, 4, 3, 3, 4, 4, 2, 2, -10};
+
+    public static int[] secondExampleIntSortedArray = {1, 5, 8, 20, 21, 26, 29, 31, 46, 79, 80, 101, 242, 333};
+    public static int[] thirdExampleIntSortedArray = {-121, -120, -40, -31, -9, 0, 5, 10, 15, 37, 40, 80, 81, 82};
+    public static int[] forthExampleIntUnSortedArray = {-99, -80, -40, 1, 3, 2004, 1995, 512, 7000, -450, 200929, -1590 - 2, 204, -100000000};
+    public static String[] fifthExampleStringArray = {"Your", "leg", "is", "very", "sexy!"};
+    public static String[] sixthExampleStringArray = {"beb", "Your", "leg", "is", "very", "sexy!", "very", "a", "a", "a"};
+
+    public static int[] seventhExampleIntSortedArray = {1, 3, 4, 5, 7};
+    public static int[] eighthExampleIntSortedArray = {2, 3, 5, 6} ;
+
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
