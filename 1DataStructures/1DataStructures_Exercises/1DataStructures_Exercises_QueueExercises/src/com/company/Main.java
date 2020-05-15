8fb6f51c668a2523896801734c407bd6adc33857
Daniel Kozak <kozak2321@gmail.com>
2020-05-15 12:29:01 +0200

    implemented stack with queues

--- 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_QueueExercises/src/com/company/Main.java
+++ 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_QueueExercises/src/com/company/Main.java
@@ -1,10 +1,29 @@
 package com.company;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        StackFromQueue stack = new StackFromQueue();
+
+        stack.push(2);
+        stack.push(3);
 
 
+//        System.out.println(stack.pop());
+        stack.push(323);
+        stack.push(324);
+        stack.push(325);
+        stack.push(326);
+
+
+
+        stack.showElements();
+
+        System.out.println("To Peck : " + stack.peek());
     }
 }
