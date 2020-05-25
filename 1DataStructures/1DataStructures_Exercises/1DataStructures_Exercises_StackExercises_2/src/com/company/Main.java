e3c547112dab0f8bd16f4b8c36471d4d1c8b2745
Daniel Kozak <kozak2321@gmail.com>
2020-05-25 13:15:58 +0200

    next exercises

--- 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_StackExercises_2/src/com/company/Main.java
+++ 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_StackExercises_2/src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Stack;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,8 +9,34 @@ public class Main {
         Collections_9_EvaluatePostfixExpression evaluator = new Collections_9_EvaluatePostfixExpression("10 2 8 * + 3 -");
 //        Collections_9_EvaluatePostfixExpression evaluator = new Collections_9_EvaluatePostfixExpression("10 2 -");
 //        Collections_9_EvaluatePostfixExpression evaluator = new Collections_9_EvaluatePostfixExpression("10");
-        System.out.println(evaluator.evaluateExpression());
+//        System.out.println(evaluator.evaluateExpression());
 
+        Collections_10_SortStackUsingAnotherTempStack sortownik = new Collections_10_SortStackUsingAnotherTempStack();
+        sortownik.sort(prepareCollections10Data());
+
+    }
+
+    public static Stack<Integer>  prepareCollections10Data(){
+
+        //    Input : [34, 3, 31, 98, 92, 23] Output : [3, 23, 31, 34, 92, 98] Input : [3, 5, 1, 4, 2, 8] Output : [1, 2, 3, 4, 5, 8]
+        Stack<Integer> inputStack1 = new Stack<Integer>();
+        inputStack1.add(34);
+        inputStack1.add(3);
+        inputStack1.add(31);
+        inputStack1.add(98);
+        inputStack1.add(92);
+        inputStack1.add(23);
+
+        Stack<Integer> inputStack2 = new Stack<Integer>();
+
+        inputStack2.add(3);
+        inputStack2.add(5);
+        inputStack2.add(1);
+        inputStack2.add(4);
+        inputStack2.add(2);
+        inputStack2.add(8);
+
+        return inputStack2;
     }
 
 
