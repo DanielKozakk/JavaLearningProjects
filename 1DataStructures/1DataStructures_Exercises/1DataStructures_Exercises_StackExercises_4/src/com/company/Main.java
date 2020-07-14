fd122c9d65182ada40f4d2ab9562a0385e72eaee
DanielKozakk <kozak2321@gmail.com>
2020-07-14 20:08:11 +0200

    col10

--- 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_StackExercises_4/src/com/company/Main.java
+++ 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_StackExercises_4/src/com/company/Main.java
@@ -1,10 +1,23 @@
 package com.company;
 
+import java.util.Stack;
+
 public class Main {
 
     public static void main(String[] args) {
+//[34, 3, 31, 98, 92, 23]
 
+        Collections_10_SortStackUsingTemporaryStack col10 = new Collections_10_SortStackUsingTemporaryStack();
+        Stack<Integer> inputStack = new Stack<>();
+        inputStack.push(34);
+        inputStack.push(3);
+        inputStack.push(31);
+        inputStack.push(98);
+        inputStack.push(92);
+        inputStack.push(1);
+        inputStack.push(23);
 
+        col10.sortingStack(inputStack);
 
 
 
