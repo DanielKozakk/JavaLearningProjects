cba714b93b434a8ba6f4cae8b216ea2883260e38
DanielKozakk <kozak2321@gmail.com>
2020-07-14 18:59:33 +0200

    prepare template for col 18

---
+++ 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_LinkedList_3/src/com/company/Collections_18_RemoveDuplicatesFromALinkedList.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Collections_18_RemoveDuplicatesFromALinkedList {
+
+
+    public void removeDuplicates(LinkedList_Template <Integer> inputLinkedList){
+
+
+
+    }
+}
--- 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_LinkedList_3/src/com/company/Main.java
+++ 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_LinkedList_3/src/com/company/Main.java
@@ -16,5 +16,26 @@ public class Main {
         Collections_17_ReturnNthElementFromTheEndLinkedList col17 = new Collections_17_ReturnNthElementFromTheEndLinkedList();
 
         col17.returnNthElement(4, ll);
+
+
+//        12->11->12->21->41->43->21
+//        12->11->21->41->43
+
+
+        LinkedList_Template<Integer> ll2 = new LinkedList_Template();
+
+        ll2.insertAtEnd(12);
+        ll2.insertAtEnd(11);
+        ll2.insertAtEnd(12);
+        ll2.insertAtEnd(21);
+        ll2.insertAtEnd(41);
+        ll2.insertAtEnd(43);
+        ll2.insertAtEnd(21);
+
+        Collections_18_RemoveDuplicatesFromALinkedList col18 = new Collections_18_RemoveDuplicatesFromALinkedList();
+
+        col18.removeDuplicates(ll2);
+
+
     }
 }
