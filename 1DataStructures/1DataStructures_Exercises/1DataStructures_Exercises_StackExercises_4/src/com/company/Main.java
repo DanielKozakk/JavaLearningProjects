37c482007af0b42579ff839a2c1cdcd5fda45985
DanielKozakk <kozak2321@gmail.com>
2020-07-14 19:36:07 +0200

    col10 start

---
+++ 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_StackExercises_4/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+//        Given a stack of integers, sort it in ascending order using another temporary stack.
+
+//        Input : [34, 3, 31, 98, 92, 23] Output : [3, 23, 31, 34, 92, 98] Input : [3, 5, 1, 4, 2, 8] Output : [1, 2, 3, 4, 5, 8]
+
+
+
+    }
+}
