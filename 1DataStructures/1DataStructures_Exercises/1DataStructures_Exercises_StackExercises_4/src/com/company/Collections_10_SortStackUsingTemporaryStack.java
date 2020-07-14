ce7631e7ef4889118e0037d9f104d9262d45fdea
DanielKozakk <kozak2321@gmail.com>
2020-07-14 19:36:37 +0200

    col 10 real start

---
+++ 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_StackExercises_4/src/com/company/Collections_10_SortStackUsingTemporaryStack.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Collections_10_SortStackUsingTemporaryStack {
+
+    //        Given a stack of integers, sort it in ascending order using another temporary stack.
+
+//        Input : [34, 3, 31, 98, 92, 23] Output : [3, 23, 31, 34, 92, 98] Input : [3, 5, 1, 4, 2, 8] Output : [1, 2, 3, 4, 5, 8]
+}
