60bbb7c77cf4debdfb6499933bd1991916d3975d
DanielKozakk <kozak2321@gmail.com>
2020-07-15 13:50:50 +0200

    exercise project

---
+++ 1DataStructures/1DataStructures_GeneralInformationAndExamples/1DataStructures_Graphs/src/com/company/AdjacencyMatrixGraph.java
@@ -0,0 +1,72 @@
+package com.company;
+
+public class AdjacencyMatrixGraph {
+    private final int numberOfVertecies;
+    private final boolean[][] adjMatrix;
+    public int verteciesRemains;
+    private static int ID = 0;
+
+    private Vertex[] listOfVertex;
+
+    public AdjacencyMatrixGraph(int numberOfVertex) {
+        this.numberOfVertecies = numberOfVertex;
+        this.verteciesRemains = numberOfVertex;
+        adjMatrix = new boolean[numberOfVertecies][numberOfVertecies];
+        listOfVertex = new Vertex[numberOfVertecies];
+    }
+
+    public void addVertex(Object dataToStore) {
+
+        if (verteciesRemains > 0) {
+            Vertex newVertex = new Vertex(ID, dataToStore);
+            listOfVertex[ID] = newVertex;
+            ID++;
+            verteciesRemains--;
+        }
+    }
+
+    // Add edges
+    public void addEdge(int i, int j) {
+
+        if (listOfVertex[i] != null && listOfVertex[j] != null) {
+
+            adjMatrix[i][j] = true;
+            adjMatrix[j][i] = true;
+        } else {
+            System.out.println("Vertex doesn't exist!");
+        }
+    }
+
+    // Remove edges
+    public void removeEdge(int i, int j) {
+        if (listOfVertex[i] != null && listOfVertex[j] != null) {
+
+            adjMatrix[i][j] = false;
+            adjMatrix[j][i] = false;
+        }
+    }
+
+    // Print the matrix
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < numberOfVertecies; i++) {
+            s.append(i + ": ");
+            for (boolean j : adjMatrix[i]) {
+                s.append((j ? 1 : 0) + " ");
+            }
+            s.append("\n");
+        }
+        return s.toString();
+    }
+
+    private class Vertex {
+        int id;
+        Object dataToStore;
+        private Vertex(int id, Object data) {
+            this.id = id;
+            this.dataToStore = data;
+        }
+    }
+
+
+}
