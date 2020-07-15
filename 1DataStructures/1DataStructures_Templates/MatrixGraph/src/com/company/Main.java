e7855c77c832337696083e5ad53b5c91160bf624
DanielKozakk <kozak2321@gmail.com>
2020-07-15 13:36:02 +0200

    implementation of adjacencyListGraph and adjacencyMatrixGraph

--- 1DataStructures/1DataStructures_Templates/MatrixGraph/src/com/company/Main.java
+++ 1DataStructures/1DataStructures_Templates/MatrixGraph/src/com/company/Main.java
@@ -4,17 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        AdjacencyMatrixGraph g = new AdjacencyMatrixGraph(4);
+        AdjacencyListGraph g = new AdjacencyListGraph(4);
 
         g.addVertex("A");
         g.addVertex("B");
         g.addVertex("C");
         g.addVertex("D");
         g.addEdge(0, 1);
-        g.addEdge(0, 2);
-        g.addEdge(1, 2);
-        g.addEdge(2, 0);
-        g.addEdge(2, 3);
+        g.addEdge(0, 1);
+        g.removeEdge(0,1);
+//        g.addEdge(0, 2);
+//        g.addEdge(1, 2);
+//        g.addEdge(2, 0);
+//        g.addEdge(2, 3);
 
         System.out.print(g.toString());
 
