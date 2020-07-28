07f58d80d9a689cbdabe991a9e18afe5d8ae7f19
Daniel Kozak <kozak2321@gmail.com>
2020-07-28 18:56:53 +0200

    col 11, remove comments

--- 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_StackExercises_3/src/com/company/Main.java
+++ 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_StackExercises_3/src/com/company/Main.java
@@ -4,29 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-
         String toEvaluate = "10 2 8 * + 3 -";
-//        String toEvaluate = "+ - * 5 *";
+
         Collections_9_EvaluatePostfixExpressionUsingAStack col9 = new Collections_9_EvaluatePostfixExpressionUsingAStack();
-//        col9.evaluate(toEvaluate);
-
-
-//        Input: exp = “[()]{}{[()()]()}”
-//        Output: Balanced
-
-
-
-
-//        Input: exp = “[(])”
-//        Output: Not Balanced
-
 
         String input_1 = "[()]{}{[()()]()}";
         String input_2 = "[(])";
 
         Collections_11_CheckForBalancedParanthesesInAnExpression col11 = new Collections_11_CheckForBalancedParanthesesInAnExpression();
         System.out.println(col11.checkForBalanced(input_2));
-
 
     }
 }
