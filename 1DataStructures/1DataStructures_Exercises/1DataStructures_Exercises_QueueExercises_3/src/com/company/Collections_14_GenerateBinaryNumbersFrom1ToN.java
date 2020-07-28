0a2d8d155154498ea3e92cfd700b5fb8b94ce879
Daniel Kozak <kozak2321@gmail.com>
2020-07-28 19:01:22 +0200

    col 14

---
+++ 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_QueueExercises_3/src/com/company/Collections_14_GenerateBinaryNumbersFrom1ToN.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Collections_14_GenerateBinaryNumbersFrom1ToN {
+
+    public void generate(int n){
+
+
+
+    }
+
+}
---
+++ 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_QueueExercises_3/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Collections_14_GenerateBinaryNumbersFrom1ToN col14 = new Collections_14_GenerateBinaryNumbersFrom1ToN();
+        col14.generate(5);
+
+    }
+}
