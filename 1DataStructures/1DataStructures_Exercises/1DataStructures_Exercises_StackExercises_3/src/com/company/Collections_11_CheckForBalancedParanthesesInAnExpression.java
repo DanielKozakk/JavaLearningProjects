048442668dda2371562d6b133aec41374cc12550
Daniel Kozak <kozak2321@gmail.com>
2020-07-28 18:56:17 +0200

    col 11

--- 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_StackExercises_3/src/com/company/Collections_11_CheckForBalancedParanthesesInAnExpression.java
+++ 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_StackExercises_3/src/com/company/Collections_11_CheckForBalancedParanthesesInAnExpression.java
@@ -1,4 +1,66 @@
 package com.company;
 
+import java.util.Stack;
+
 public class Collections_11_CheckForBalancedParanthesesInAnExpression {
+
+    public boolean checkForBalanced(String input){
+
+//        Input: exp = “[()]{}{[()()]()}”
+//        Output: Balanced
+
+
+//        Input: exp = “[(])”
+//        Output: Not Balanced
+
+        /*
+
+                Iteruje się przez elementy Stringa
+                Jak natrafię na nawias otwierający, dodaję go do stacka
+                Jak natrafię na nawias zamykający, to sprawdzam, czy pop odpowiada nawiasowi
+                Jeśli nie, to przerywam pętlę i robię że jest nie zbalansowany
+
+                Na koniec sprawdzam, zwracam, czy stack jest pusty
+
+         */
+
+        Stack<String> outputStack = new Stack<>();
+        boolean isStackBalanced = true;
+
+        for(int i = 0; i < input.length(); i ++ ){
+
+            String ch = String.valueOf(input.charAt(i));
+
+            switch (ch){
+                case "{":
+                case "(":
+                case "[":
+                    outputStack.push(ch);
+                    break;
+
+                case ")":
+                    if(!outputStack.pop().equals("(")){
+                        isStackBalanced = false;
+                    }
+                    break;
+                case "]":
+                    if(!outputStack.pop().equals("[")){
+                        isStackBalanced = false;
+                    }
+                    break;
+                case "}":
+                    if(!outputStack.pop().equals("{")){
+                        isStackBalanced = false;
+                    }
+                    break;
+            }
+
+            if(!isStackBalanced) return isStackBalanced;
+
+        }
+
+        return outputStack.isEmpty();
+
+
+    }
 }
--- 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_StackExercises_3/src/com/company/Main.java
+++ 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_StackExercises_3/src/com/company/Main.java
@@ -8,7 +8,25 @@ public class Main {
         String toEvaluate = "10 2 8 * + 3 -";
 //        String toEvaluate = "+ - * 5 *";
         Collections_9_EvaluatePostfixExpressionUsingAStack col9 = new Collections_9_EvaluatePostfixExpressionUsingAStack();
-        col9.evaluate(toEvaluate);
+//        col9.evaluate(toEvaluate);
+
+
+//        Input: exp = “[()]{}{[()()]()}”
+//        Output: Balanced
+
+
+
+
+//        Input: exp = “[(])”
+//        Output: Not Balanced
+
+
+        String input_1 = "[()]{}{[()()]()}";
+        String input_2 = "[(])";
+
+        Collections_11_CheckForBalancedParanthesesInAnExpression col11 = new Collections_11_CheckForBalancedParanthesesInAnExpression();
+        System.out.println(col11.checkForBalanced(input_2));
+
 
     }
 }
