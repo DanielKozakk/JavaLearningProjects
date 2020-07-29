e915504d8786422dbfa0345512852e43ade17d49
Daniel Kozak <kozak2321@gmail.com>
2020-07-29 19:51:54 +0200

    col 15

--- 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_LinkedList_4/src/com/company/Collections_15_ReverseALinkedList.java
+++ 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_LinkedList_4/src/com/company/Collections_15_ReverseALinkedList.java
@@ -1,7 +1,67 @@
 package com.company;
 
-public class Collections_15_ReverseALinkedList {
+public class Collections_15_ReverseALinkedList<E> {
+
+    public void reverse(LinkedListTemplate<E> inputLinkedList) {
+
+        /*
+            na papierze:
+
+            1->2->3->4->NULL
+
+            1->NULL, 2->3->4
+            2->1->NULL, 3->4
+
+            prev = null
+            curr = first
+            next = first.getNext
 
 
+            iteracje pętli while:
+
+
+         */
+
+        LinkedListTemplate<E>.Node prev = null;
+        LinkedListTemplate<E>.Node curr = inputLinkedList.getFirstElement();
+        LinkedListTemplate<E>.Node next = curr != null ? curr.getNext() : null;
+
+
+
+        while(curr != null){
+
+            /*
+            1->2->3->4->NULL
+
+            1->NULL, 2->3->4
+            2->1->NULL, 3->4
+
+             */
+
+
+            curr.setNext(prev);
+
+               // 1->NULL
+
+
+            prev = curr;
+            // prev = 1;
+            curr = next;
+            // curr = 2
+            next = curr != null ? curr.getNext() : null;
+            // next = 3
+            if(curr == null) inputLinkedList.setHead(prev);
+
+
+
+
+
+
+
+        }
+
+        inputLinkedList.printLinkedList();
+
+    }
 
 }
--- 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_LinkedList_4/src/com/company/Main.java
+++ 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_LinkedList_4/src/com/company/Main.java
@@ -4,7 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 
+    LinkedListTemplate<Integer> inputLinkedList = new LinkedListTemplate<>();
+    inputLinkedList.insertAtEnd(1);
+    inputLinkedList.insertAtEnd(2);
+    inputLinkedList.insertAtEnd(3);
+    inputLinkedList.insertAtEnd(4);
+    inputLinkedList.insertAtEnd(5);
 
+
+
+    Collections_15_ReverseALinkedList<Integer> collections_15_reverseALinkedList = new Collections_15_ReverseALinkedList<>();
+
+    collections_15_reverseALinkedList.reverse(inputLinkedList);
 
     }
 }
