879702c68ed3d91a6d1cd8e6782b39d3af802c07
Daniel Kozak <kozak2321@gmail.com>
2020-07-29 19:11:50 +0200

    col 12

--- 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_StackExercises_3/src/com/company/Collections_12_ImplementUsingQueue.java
+++ 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_StackExercises_3/src/com/company/Collections_12_ImplementUsingQueue.java
@@ -1,23 +1,35 @@
 package com.company;
 
+import java.util.LinkedList;
 import java.util.Queue;
 
 public class Collections_12_ImplementUsingQueue <E>   {
 
-//    A stack can be implemented using two queues.
-//
-//    pop
-//            push
-//    peek
-//            size
+    Queue<E> queue1 = new LinkedList<>();
+    Queue<E> queue2 = new LinkedList<>();
 
+    public void push(E elementToAdd){
 
+        queue2.add(elementToAdd);
 
-    Collections_12_ImplementUsingQueue(){
+        while(!queue1.isEmpty()){
+            queue2.add(queue1.poll());
+        }
 
-
-
+        queue1 = queue2;
+        queue2 = new LinkedList<>();
     }
 
+    public E pop(){
+        return queue1.poll();
+    }
+
+    public E peek(){
+        return queue1.peek();
+    }
+
+    public int size(){
+        return queue1.size();
+    }
 
 }
--- 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_StackExercises_3/src/com/company/Main.java
+++ 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_StackExercises_3/src/com/company/Main.java
@@ -14,7 +14,14 @@ public class Main {
         Collections_11_CheckForBalancedParanthesesInAnExpression col11 = new Collections_11_CheckForBalancedParanthesesInAnExpression();
 //        System.out.println(col11.checkForBalanced(input_2));
 
-        Collections_12_ImplementUsingQueue<String> col12 = new Collections_12_ImplementUsingQueue();
+        Collections_12_ImplementUsingQueue<Integer> col12 = new Collections_12_ImplementUsingQueue<>();
+
+        col12.push(1);
+        col12.push(2);
+//        col12.push(3);
+
+        System.out.println(col12.size());
+
     }
 
 
