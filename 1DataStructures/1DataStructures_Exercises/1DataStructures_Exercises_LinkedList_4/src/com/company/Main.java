fd292584255f8ad8636857317834b9be8f65a6e8
Daniel Kozak <kozak2321@gmail.com>
2020-07-30 18:46:11 +0200

    col18

--- 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_LinkedList_4/src/com/company/Main.java
+++ 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_LinkedList_4/src/com/company/Main.java
@@ -6,17 +6,26 @@ public class Main {
 
     LinkedListTemplate<Integer> inputLinkedList = new LinkedListTemplate<>();
     inputLinkedList.insertAtEnd(1);
+    inputLinkedList.insertAtEnd(1);
+    inputLinkedList.insertAtEnd(1);
+    inputLinkedList.insertAtEnd(1);
+    inputLinkedList.insertAtEnd(1);
     inputLinkedList.insertAtEnd(2);
     inputLinkedList.insertAtEnd(3);
-    inputLinkedList.insertAtEnd(4);
     inputLinkedList.insertAtEnd(5);
+    inputLinkedList.insertAtEnd(5);
+    inputLinkedList.insertAtEnd(2);
+    inputLinkedList.insertAtEnd(4);
 
 
 
     Collections_15_ReverseALinkedList<Integer> collections_15_reverseALinkedList = new Collections_15_ReverseALinkedList<>();
 
 //    collections_15_reverseALinkedList.reverse(inputLinkedList);
-    Collections_17_ReturnNthElementFromEndOfLinkedList col17 = new Collections_17_ReturnNthElementFromEndOfLinkedList();
-    col17.returnNthElemenbt(5, inputLinkedList);
+//    Collections_17_ReturnNthElementFromEndOfLinkedList col17 = new Collections_17_ReturnNthElementFromEndOfLinkedList();
+//    col17.returnNthElemenbt(5, inputLinkedList);
+
+        Collections_18_RemoveDuplicatesFromLinkedList col18 = new Collections_18_RemoveDuplicatesFromLinkedList();
+        col18.remove(inputLinkedList);
     }
 }
