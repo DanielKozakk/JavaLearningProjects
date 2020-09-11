42d6b4b895481b03822d7b71148537df64f6c83e
Daniel Kozak <kozak2321@gmail.com>
2020-09-11 09:07:23 +0200

    create new col 19 file

---
+++ 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_Graph_1/src/com/company/Collections_19_BreadthFirstSearchInAdjacencyMatrix.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Collections_19_BreadthFirstSearchInAdjacencyMatrix extends AdjacencyMatrixDirectedRawGraph{
+
+    Collections_19_BreadthFirstSearchInAdjacencyMatrix(Integer graphSize) {
+        super(graphSize);
+    }
+
+
+}
