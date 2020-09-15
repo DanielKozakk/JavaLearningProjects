d5c3a7de30c8e16510b3ce2d95a1dbb19c654e5d
Daniel Kozak <kozak2321@gmail.com>
2020-09-15 10:03:06 +0200

    col 22

---
+++ 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_Graph_1/src/com/company/Collections_22_DepthfirstSearchInAdjacencyList.java
@@ -0,0 +1,78 @@
+package com.company;
+
+import java.util.Stack;
+
+public class Collections_22_DepthfirstSearchInAdjacencyList extends AdjacencyListDirectedRawGraph {
+
+
+    public Collections_22_DepthfirstSearchInAdjacencyList(int graphSize) {
+        super(graphSize);
+    }
+
+    public void depthFirstSearch() {
+
+        /*
+
+        Robię verticiesStack
+        Robię searchedVertex jako pierwszy
+        dodaję searched do stacka
+
+        while stack nie jest pusty
+
+            wypisuję wartość searchedStack
+            oznaczam jako visited
+            robię bool czy jest on exhausted
+
+            for loop poprzez sąsiadów
+
+                jeśli sąsiad nie jest visited
+
+                    exhausted = false
+                    searched verex = sąsiad
+            endforloop
+
+            if isexhausted
+
+                stack.pop
+                if !stack.isEmpty
+
+                    searched = stack.peek
+         */
+
+        Stack<Vertex> stack = new Stack<>();
+        Vertex searchedVertex = vertices[0];
+        stack.add(searchedVertex);
+        searchedVertex.setVertexAsVisited();
+        System.out.println(searchedVertex.ID);
+
+        while(!stack.isEmpty()){
+
+
+
+            if(!searchedVertex.isVertexVisited){
+                System.out.println(searchedVertex.ID);
+                stack.add(searchedVertex);
+            }
+            searchedVertex.setVertexAsVisited();
+
+            boolean isVertexExhausted = true;
+
+            for(Integer neighbourId : searchedVertex.listOfEdges){
+
+
+                if(!vertices[neighbourId].isVertexVisited){
+                    isVertexExhausted=false;
+                    searchedVertex = vertices[neighbourId];
+                    break;
+                }
+            }
+            if(isVertexExhausted){
+                stack.pop();
+                if(!stack.isEmpty()){
+                    searchedVertex=stack.peek();
+                }
+            }
+        }
+
+    }
+}
--- 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_Graph_1/src/com/company/Main.java
+++ 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_Graph_1/src/com/company/Main.java
@@ -6,36 +6,36 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Collections_21_BreadthFirstSearchInAdjacencyList col21 = new Collections_21_BreadthFirstSearchInAdjacencyList(7);
-        col21.addVertex("Pierwszy");
-        col21.addVertex("Drugi");
-        col21.addVertex("Trzeci");
-        col21.addVertex("Czwarty");
-        col21.addVertex("Piąty");
-        col21.addVertex("Szósty");
-        col21.addVertex("Siódmyyyy");
+        Collections_22_DepthfirstSearchInAdjacencyList col22 = new Collections_22_DepthfirstSearchInAdjacencyList(7);
+        col22.addVertex("Pierwszy");
+        col22.addVertex("Drugi");
+        col22.addVertex("Trzeci");
+        col22.addVertex("Czwarty");
+        col22.addVertex("Piąty");
+        col22.addVertex("Szósty");
+        col22.addVertex("Siódmyyyy");
 
 
 
-        col21.addEdge(0, 1);
-        col21.addEdge(1, 0);
+        col22.addEdge(0, 1);
+        col22.addEdge(1, 0);
 
-        col21.addEdge(0, 2);
-        col21.addEdge(2, 0);
+        col22.addEdge(0, 2);
+        col22.addEdge(2, 0);
 
-        col21.addEdge(2, 3);
-        col21.addEdge(3, 2);
+        col22.addEdge(2, 3);
+        col22.addEdge(3, 2);
 
-        col21.addEdge(2, 4);
-        col21.addEdge(4, 2);
+        col22.addEdge(2, 4);
+        col22.addEdge(4, 2);
 
-        col21.addEdge(3, 5);
-        col21.addEdge(5, 3);
+        col22.addEdge(3, 5);
+        col22.addEdge(5, 3);
 
-        col21.addEdge(1, 6);
-        col21.addEdge(6, 1);
+        col22.addEdge(1, 6);
+        col22.addEdge(6, 1);
 
-        col21.breadthFirstSearchInAdjacencyList();
+        col22.depthFirstSearch();
     }
 
 
