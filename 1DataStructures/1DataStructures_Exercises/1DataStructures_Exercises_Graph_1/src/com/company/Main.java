42099a59bf361df7083fcda14393a4b26e9fc8ec
Daniel Kozak <kozak2321@gmail.com>
2020-09-21 16:44:14 +0200

    col20

--- 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_Graph_1/src/com/company/Main.java
+++ 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_Graph_1/src/com/company/Main.java
@@ -2,40 +2,36 @@ package com.company;
 
 
 import com.company.serie2.Collections_19_ImplementBreadthFirstSearch;
+import com.company.serie2.Collections_20_ImplementDepthFirstSearchInAdjacencyMatrix;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Collections_19_ImplementBreadthFirstSearch col19_2 = new Collections_19_ImplementBreadthFirstSearch(7);
 
-        col19_2.addVertex("Pierwszy");
-        col19_2.addVertex("Drugi");
-        col19_2.addVertex("Trzeci");
-        col19_2.addVertex("Czwarty");
-        col19_2.addVertex("Piąty");
-        col19_2.addVertex("Szósty");
-        col19_2.addVertex("Siódmyyyy");
+        Collections_20_ImplementDepthFirstSearchInAdjacencyMatrix col20_2 = new Collections_20_ImplementDepthFirstSearchInAdjacencyMatrix(7);
+        col20_2.addVertex("Pierwszy");
+        col20_2.addVertex("Drugi");
+        col20_2.addVertex("Trzeci");
+        col20_2.addVertex("Czwarty");
+        col20_2.addVertex("Piąty");
+        col20_2.addVertex("Szósty");
+        col20_2.addVertex("Siódmyyyy");
+        col20_2.addEdge(0, 1);
+        col20_2.addEdge(1, 0);
+        col20_2.addEdge(0, 2);
+        col20_2.addEdge(2, 0);
+        col20_2.addEdge(2, 3);
+        col20_2.addEdge(3, 2);
+        col20_2.addEdge(2, 4);
+        col20_2.addEdge(4, 2);
+        col20_2.addEdge(3, 5);
+        col20_2.addEdge(5, 3);
+        col20_2.addEdge(1, 6);
+        col20_2.addEdge(6, 1);
 
-        col19_2.addEdge(0, 1);
-        col19_2.addEdge(1, 0);
+        col20_2.implementDepthFirstSearch();
 
-        col19_2.addEdge(0, 2);
-        col19_2.addEdge(2, 0);
-
-        col19_2.addEdge(2, 3);
-        col19_2.addEdge(3, 2);
-
-        col19_2.addEdge(2, 4);
-        col19_2.addEdge(4, 2);
-
-        col19_2.addEdge(3, 5);
-        col19_2.addEdge(5, 3);
-
-        col19_2.addEdge(1, 6);
-        col19_2.addEdge(6, 1);
-
-        col19_2.breadthFirstSearch();
 
 
     }
