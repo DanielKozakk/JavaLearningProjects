0fd35a476225e7b5d2f297f21d268d5dcf1c92ff
Daniel Kozak <kozak2321@gmail.com>
2020-09-21 17:04:14 +0200

    initial files for future

---
+++ 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_Graph_1/src/com/company/serie2/Collections_22_ImplementDepthFirstSearchInAdjacencyList.java
@@ -0,0 +1,4 @@
+package com.company.serie2;
+
+public class Collections_22_ImplementDepthFirstSearchInAdjacencyList {
+}
---
+++ 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_Trees_1/src/com/company/serie1/Collections_26_ImplementBinarySearchTree.java
@@ -0,0 +1,4 @@
+package com.company.serie1;
+
+public class Collections_26_ImplementBinarySearchTree {
+}
