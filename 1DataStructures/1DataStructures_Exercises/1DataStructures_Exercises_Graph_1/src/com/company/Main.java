4714cde8133d8bc0d4f4b068268738edb8e3176f
Daniel Kozak <kozak2321@gmail.com>
2020-09-22 10:00:05 +0200

    col 22

--- 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_Graph_1/src/com/company/Main.java
+++ 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_Graph_1/src/com/company/Main.java
@@ -4,46 +4,36 @@ package com.company;
 import com.company.serie2.Collections_19_ImplementBreadthFirstSearch;
 import com.company.serie2.Collections_20_ImplementDepthFirstSearchInAdjacencyMatrix;
 import com.company.serie2.Collections_21_ImplementBreadthFirstSearchInAdjacencyList;
+import com.company.serie2.Collections_22_ImplementDepthFirstSearchInAdjacencyList;
 
 public class Main {
 
     public static void main(String[] args) {
 
 
-        Collections_21_ImplementBreadthFirstSearchInAdjacencyList col21_2 = new Collections_21_ImplementBreadthFirstSearchInAdjacencyList(7);
-        col21_2.addVertex("Pierwszy");
-        col21_2.addVertex("Drugi");
-        col21_2.addVertex("Trzeci");
-        col21_2.addVertex("Czwarty");
-        col21_2.addVertex("Piąty");
-        col21_2.addVertex("Szósty");
-        col21_2.addVertex("Siódmyyyy");
+        Collections_22_ImplementDepthFirstSearchInAdjacencyList col22_2 = new Collections_22_ImplementDepthFirstSearchInAdjacencyList(7);
+        col22_2.addVertex("Pierwszy");
+        col22_2.addVertex("Drugi");
+        col22_2.addVertex("Trzeci");
+        col22_2.addVertex("Czwarty");
+        col22_2.addVertex("Piąty");
+        col22_2.addVertex("Szósty");
+        col22_2.addVertex("Siódmyyyy");
+        col22_2.addEdge(0, 1);
+        col22_2.addEdge(1, 0);
+        col22_2.addEdge(0, 2);
+        col22_2.addEdge(2, 0);
+        col22_2.addEdge(2, 3);
+        col22_2.addEdge(3, 2);
+        col22_2.addEdge(2, 4);
+        col22_2.addEdge(4, 2);
+        col22_2.addEdge(3, 5);
+        col22_2.addEdge(5, 3);
+        col22_2.addEdge(1, 6);
+        col22_2.addEdge(6, 1);
 
 
-
-        col21_2.addEdge(0, 1);
-        col21_2.addEdge(1, 0);
-
-        col21_2.addEdge(0, 2);
-        col21_2.addEdge(2, 0);
-
-
-        col21_2.addEdge(2, 3);
-        col21_2.addEdge(3, 2);
-
-        col21_2.addEdge(2, 4);
-        col21_2.addEdge(4, 2);
-
-        col21_2.addEdge(3, 5);
-        col21_2.addEdge(5, 3);
-
-        col21_2.addEdge(1, 6);
-        col21_2.addEdge(6, 1);
-
-        col21_2.breadthFirstSearchInAdjacencyList();
-
-
-
+        col22_2.depthFirstSearch();
     }
 
 
--- 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_Graph_1/src/com/company/serie2/Collections_22_ImplementDepthFirstSearchInAdjacencyList.java
+++ 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_Graph_1/src/com/company/serie2/Collections_22_ImplementDepthFirstSearchInAdjacencyList.java
@@ -1,4 +1,76 @@
 package com.company.serie2;
 
-public class Collections_22_ImplementDepthFirstSearchInAdjacencyList {
+import com.company.AdjacencyListDirectedRawGraph;
+
+import java.util.Stack;
+
+public class Collections_22_ImplementDepthFirstSearchInAdjacencyList extends AdjacencyListDirectedRawGraph {
+    public Collections_22_ImplementDepthFirstSearchInAdjacencyList(int graphSize) {
+        super(graphSize);
+    }
+    public void depthFirstSearch(){
+
+
+        /*
+
+            Robię stack vertex
+            Robię searched na pierwszy
+
+            dodaję do stacka
+
+            robię pętlę, sprawdza czy stack nie jest pusty
+
+                jeśli vertex is not visited
+                    wypisz wartość
+
+                oznacz jako visited
+                zmienna czy vertex is exhausted = true
+
+                idź po sąsiadach
+
+                    jeśli jest sąsiad, który jest nieodwiedzony i nie ma go w stakcu
+                    isVertexExhausted = false
+                    searched vertex = ten sąsiad
+                    break
+
+                jeśli is vertexexhausted
+
+                    robię stack.pop
+                    jeśli nie jest pusty to sarched = stack.peek
+
+         */
+
+        Stack<Vertex> verticesStack = new Stack<>();
+        Vertex searchedVertex = vertices[0];
+
+        verticesStack.add(searchedVertex);
+
+        while(!verticesStack.isEmpty()){
+
+            if(!searchedVertex.isVertexVisited()){
+                System.out.println(searchedVertex.getId());
+            }
+            searchedVertex.setVertexAsVisited();
+            boolean isVertexExhausted = true;
+            for(int i = 0 ; i < searchedVertex.listOfEdges.size() ; i++){
+                Integer neighbourId = searchedVertex.listOfEdges.get(i);
+                Vertex neighbour = vertices[neighbourId];
+                if(!neighbour.isVertexVisited() && !verticesStack.contains(neighbour)){
+                    verticesStack.add(neighbour);
+                    isVertexExhausted =false;
+                    searchedVertex = neighbour;
+                    break;
+                }
+            }
+            if(isVertexExhausted){
+                verticesStack.pop();
+                if(!verticesStack.isEmpty()){
+                    searchedVertex = verticesStack.peek();
+                }
+            }
+
+        }
+
+
+    }
 }
