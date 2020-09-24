2b4f1cd5a2c909bb490a6e88412a2f4e6c35a5c0
Daniel Kozak <kozak2321@gmail.com>
2020-09-24 09:53:39 +0200

    col 28

--- 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_Trees_1/src/com/company/Main.java
+++ 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_Trees_1/src/com/company/Main.java
@@ -2,25 +2,26 @@ package com.company;
 
 import com.company.serie1.Collections_26_ImplementBinarySearchTree;
 import com.company.serie1.Collections_27_FindHeightOfABinaryTree;
+import com.company.serie1.Collections_28_FindKthMaximumValueInTrie;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Collections_27_FindHeightOfABinaryTree col27 = new Collections_27_FindHeightOfABinaryTree(10);
+        Collections_28_FindKthMaximumValueInTrie col28 = new Collections_28_FindKthMaximumValueInTrie(10);
 
-        col27.insert(15);
-        col27.insert(5);
-        col27.insert(8);
-        col27.insert(7);
-        col27.insert(4);
-        col27.insert(12);
-        col27.insert(16);
+        col28.insert(15);
+        col28.insert(5);
+        col28.insert(8);
+        col28.insert(7);
+        col28.insert(4);
+        col28.insert(12);
+        col28.insert(16);
 
-        col27.insert(17);
-        col27.insert(18);
+//        System.out.println(Collections_28_FindKthMaximumValueInTrie.size);
 
-        col27.findHeight();
+        col28.findKthMinimumValue(8);
+
 
     }
 }
