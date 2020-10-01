e2ec22e1522a7b1f0cfed4acf2800d65fcd2b765
Daniel Kozak <kozak2321@gmail.com>
2020-10-01 10:20:10 +0200

    col 29

--- 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_Trees_1/src/com/company/Main.java
+++ 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_Trees_1/src/com/company/Main.java
@@ -1,6 +1,5 @@
 package com.company;
 
-import com.company.serie1.Collections_26_ImplementBinarySearchTree;
 import com.company.serie2.*;
 
 public class Main {
@@ -8,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-        Collections_28_FindKthMaximumValueInABinarySearchTree col28 = new Collections_28_FindKthMaximumValueInABinarySearchTree();
+        Collections_29_FindNodesAtKDistanceFromTheRoot col28 = new Collections_29_FindNodesAtKDistanceFromTheRoot();
 
         col28.insert(10);
         col28.insert(15);
@@ -19,7 +18,7 @@ public class Main {
         col28.insert(12);
         col28.insert(16);
 
-        col28.findKthMaximumValue(1);
+        col28.findNodesAtKDistance(3);
 
     }
 }
