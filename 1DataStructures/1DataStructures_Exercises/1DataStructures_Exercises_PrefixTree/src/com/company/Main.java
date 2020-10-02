d452db6d8de2ad53556e35bc31f18ff97f03fb20
Daniel Kozak <kozak2321@gmail.com>
2020-10-02 08:44:21 +0200

    collection 33

--- 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_PrefixTree/src/com/company/Main.java
+++ 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_PrefixTree/src/com/company/Main.java
@@ -1,22 +1,18 @@
 package com.company;
 
-import com.company.serie1.Collections_31_ImplementPrefixTree;
-import com.company.serie1.Collections_32_PrintAllWordsStoredInPrefixTree;
-import com.company.serie1.Collections_33_FormWordsFromADictionaryUsingPrefixTree;
+import com.company.serie1.Collections_33_ImplementDictionaryUsingTrie;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Collections_33_FormWordsFromADictionaryUsingPrefixTree col33 = new Collections_33_FormWordsFromADictionaryUsingPrefixTree();
+        Collections_33_ImplementDictionaryUsingTrie col33 = new Collections_33_ImplementDictionaryUsingTrie();
 
-        col33.add("ma", "Gatunek ptaka z rodziny ruchowatych");
-        col33.add("m", "Miesiąc przed kwietniem.");
-       String def = col33.findDefinition("majeranek");
-       String def2 = col33.findDefinition("maj");
+        col33.add("majeranek", "Gatunek ptaka z rodziny ruchowatych");
+        col33.add("maj", "Miesiąc przed kwietniem.");
+       String def = col33.findDefinition("majerane");
 
        System.out.println(def);
-       System.out.println(def2);
 
     }
 }
