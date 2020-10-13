aca89c1372acfeab31d80d6b8a198ef80353ca11
Daniel Kozak <kozak2321@gmail.com>
2020-10-13 14:24:43 +0200

    collections 25

--- 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_Graph_1/src/com/company/Main.java
+++ 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_Graph_1/src/com/company/Main.java
@@ -7,79 +7,24 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Collections_25_FindShortestPathBetweenTwoVertices col25 = new Collections_25_FindShortestPathBetweenTwoVertices(5);
 
 
+        col25.addVertex("A");//0
+        col25.addVertex("B");//1
+        col25.addVertex("C");//2
+        col25.addVertex("D");//3
+        col25.addVertex("E");//4
 
-        Collections_23_CheckIfGraphIsATreeOrNot col23 = new Collections_23_CheckIfGraphIsATreeOrNot(7);
-        col23.addVertex("Zerowy");
-        col23.addVertex("Pierwszy");
-        col23.addVertex("Drugi");
-        col23.addVertex("Trzeci");
-        col23.addVertex("Czwarty");
-        col23.addVertex("Piąty");
-        col23.addVertex("Szósty");
+        col25.addEdge(0, 1, 6);
+        col25.addEdge(0, 3, 1);
+        col25.addEdge(1, 3, 2);
+        col25.addEdge(1, 4, 2);
+        col25.addEdge(1, 2, 5);
+        col25.addEdge(2, 4, 5);
+        col25.addEdge(3, 4, 1);
 
-        col23.addEdge(0, 1);
-        col23.addEdge(1, 0);
-
-        col23.addEdge(0, 2);
-        col23.addEdge(2, 0);
-
-
-        col23.addEdge(1,6);
-        col23.addEdge(6,1);
-
-        col23.addEdge(2,3);
-        col23.addEdge(3,2);
-
-        col23.addEdge(2,4);
-        col23.addEdge(4,2);
-
-        col23.addEdge(5,3);
-        col23.addEdge(3,5);
-
-//        System.out.println(col23.checkIfGraphIsTree());
-
-        Collections_23_CheckIfGraphIsATreeOrNot col23_2 = new Collections_23_CheckIfGraphIsATreeOrNot(8);
-        col23_2.addVertex("Zerowy");
-        col23_2.addVertex("Pierwszy");
-        col23_2.addVertex("Drugi");
-        col23_2.addVertex("Trzeci");
-        col23_2.addVertex("Czwarty");
-        col23_2.addVertex("Piąty");
-        col23_2.addVertex("Szósty");
-        col23_2.addVertex("Siódmy");
-
-
-        col23_2.addEdge(0, 1);
-        col23_2.addEdge(1, 0);
-
-        col23_2.addEdge(0, 2);
-        col23_2.addEdge(2, 0);
-
-        col23_2.addEdge(1, 2);
-        col23_2.addEdge(2, 1);
-
-        col23_2.addEdge(3, 2);
-        col23_2.addEdge(2, 3);
-
-        col23_2.addEdge(4, 2);
-        col23_2.addEdge(2, 4);
-
-        col23_2.addEdge(3, 5);
-        col23_2.addEdge(5, 3);
-
-        col23_2.addEdge(1, 7);
-        col23_2.addEdge(7, 1);
-
-        col23_2.addEdge(1, 6);
-        col23_2.addEdge(6, 1);
-
-        col23_2.addEdge(7, 6);
-        col23_2.addEdge(6, 7);
-
-        System.out.println(col23_2.checkIfGraphIsTree());
-
+        col25.findShortestPath(0,2);
 
     }
 
