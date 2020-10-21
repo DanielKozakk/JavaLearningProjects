7eeba4202c3e02ab14f697d26dc08871b5de6b65
Daniel Kozak <kozak2321@gmail.com>
2020-10-21 12:23:47 +0200

    refactor 2

--- 2ObjectOrientedProgramming/src/com/company/Main.java
+++ 2ObjectOrientedProgramming/src/com/company/Main.java
@@ -1,7 +1,12 @@
 package com.company;
 
+import com.company.drive.Drive;
 import com.company.drive.HDDDrive;
 import com.company.drive.SDDDrive;
+import com.company.file.File;
+import com.company.file.imagefile.GIFImageFile;
+import com.company.file.imagefile.JPGImageFile;
+import com.company.file.musicfile.MP3File;
 import com.company.usbdevice.MemoryStick;
 import com.company.usbdevice.Mouse;
 
@@ -13,22 +18,26 @@ public class Main {
 
 
         Monitor monitor = new Monitor();
-        HDDDrive drive = new HDDDrive();
+        Drive drive = new HDDDrive();
 
         Computer computer = new Computer(monitor, drive);
 
-//        drive.addFile(new File("jnsp.jpg"));
-//
+        GIFImageFile pierwszyGif = new GIFImageFile("pierwszyGif", 200);
+        JPGImageFile pierwszJPG = new JPGImageFile("pierwszJPG", 20, 80);
+
+        MP3File mp3File = new MP3File("pierwszyMP3", 4000, "Marlin Manson", "Broken Needle", 20);
+
+
+        drive.addFile(pierwszJPG);
+        drive.addFile(pierwszyGif);
+        drive.addFile(mp3File);
+
 //        drive.listFiles();
+        File pierwszyMP3 = drive.findFile("pierwszyMP3");
+        System.out.println(pierwszyMP3.getSize());
 
-        MemoryStick pendrive = new MemoryStick("Pendrive");
-        Mouse mouse = new Mouse("Mouse");
 
-        computer.addUSBDevice(pendrive);
-//        computer.addUSBDevice(mouse);
+        // 2:20:09
 
-        pendrive.eject();
-        computer.removeUSBDevice(pendrive);
-//        computer.removeUSBDevice(mouse);
     }
 }
