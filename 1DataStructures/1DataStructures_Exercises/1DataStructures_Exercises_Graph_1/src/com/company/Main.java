40358c876c02e0d5e647b9c18aee93d586c1756b
Daniel Kozak <kozak2321@gmail.com>
2020-11-05 08:52:02 +0100

    col 24

--- 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_Graph_1/src/com/company/Main.java
+++ 1DataStructures/1DataStructures_Exercises/1DataStructures_Exercises_Graph_1/src/com/company/Main.java
@@ -3,30 +3,31 @@ package com.company;
 
 import com.company.serie3.*;
 import com.company.serie4.Collections_23_CheckIfGraphIsATree;
+import com.company.serie4.Collections_24_CountTheNumberOfEdgesInAGraph;
 import com.company.serie4.Collections_25_FindShortestPath;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Collections_25_FindShortestPath col25 = new Collections_25_FindShortestPath(5);
+        Collections_24_CountTheNumberOfEdgesInAGraph col24 = new Collections_24_CountTheNumberOfEdgesInAGraph(5);
 
 
-        col25.addVertex("A");//0
-        col25.addVertex("B");//1
-        col25.addVertex("C");//2
-        col25.addVertex("D");//3
-        col25.addVertex("E");//4
+        col24.addVertex("A");//0
+        col24.addVertex("B");//1
+        col24.addVertex("C");//2
+        col24.addVertex("D");//3
+        col24.addVertex("E");//4
 
-        col25.addEdge(0, 1, 6);
-        col25.addEdge(0, 3, 1);
-        col25.addEdge(1, 3, 2);
-        col25.addEdge(1, 4, 2);
-        col25.addEdge(1, 2, 5);
-        col25.addEdge(2, 4, 5);
-        col25.addEdge(3, 4, 1);
+        col24.addEdge(0, 1, 6);
+        col24.addEdge(0, 3, 1);
+        col24.addEdge(1, 3, 2);
+        col24.addEdge(1, 4, 2);
+        col24.addEdge(1, 2, 5);
+        col24.addEdge(2, 4, 5);
+        col24.addEdge(3, 4, 1);
 
-        col25.findShortestPath(0, 2);
+        System.out.println(col24.countEdges());
 
     }
 
