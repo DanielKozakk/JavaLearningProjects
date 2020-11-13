c88c410ca366c258923b002b4cda7297cff4d120
Daniel Kozak <kozak2321@gmail.com>
2020-11-13 13:32:27 +0100

    recursive for quick sort

--- 3Algorithms/src/com/company/Main.java
+++ 3Algorithms/src/com/company/Main.java
@@ -1,61 +1,21 @@
 package com.company;
 
-import com.company.problempudelka.Box;
-import com.company.problempudelka.Item;
-import com.company.problempudelka.Key;
+import com.company.sumazrekurencja.Rekurencja;
 
-import java.util.Stack;
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Box mainBox = new Box();
+        var input = new ArrayList<Integer>();
+        input.add(2);
+        input.add(3);
+        input.add(22);
+        input.add(22222);
+        input.add(22);
 
-        Box box1 = new Box();
-        Box box2 = new Box();
-        Box box3 = new Box();
-        Box box4 = new Box();
-
-
-        box1.addToBox(new Box());
-        box1.addToBox(new Box());
-        box1.addToBox(new Box());
-        box1.addToBox(new Box());
-
-        box2.addToBox(new Box());
-
-        var luckyBox = new Box();
-        luckyBox.addToBox(new Key());
-        box4.addToBox(luckyBox);
-
-        mainBox.addToBox(box1);
-        mainBox.addToBox(box2);
-        mainBox.addToBox(box3);
-        mainBox.addToBox(box4);
-
-
-        Stack<Box> stosPudelek = new Stack<>();
-        stosPudelek.add(mainBox);
-
-        mainBox.lookForKey(mainBox);
-
-//        while(!stosPudelek.isEmpty()){
-//            Box searched = stosPudelek.pop();
-//
-//            System.out.println("Przeszukuję pudełko o id : "  + searched.boxID);
-//
-//            for(Item item : searched.itemsArrayList){
-//                if(item instanceof Box){
-//                    stosPudelek.add((Box) item);
-//                } else {
-//                    System.out.println("znalazłem klucz!");
-//                    break;
-//                }
-//            }
-//        }
-
-
+        System.out.println(Rekurencja.highestNumber(input));
 
     }
 }
