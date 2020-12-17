bbe490b294964e683cfc87cd7601a886cddf8d07
Daniel Kozak <kozak2321@gmail.com>
2020-12-17 11:32:15 +0100

    algorithms 5 quick sort empty class

--- 3Algorithms/3Algorithms_Exercises/src/com/company/Main.java
+++ 3Algorithms/3Algorithms_Exercises/src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 import com.company.serie1.Algorithms_1_BinarySearch;
 import com.company.serie1.Algorithms_2_SelectionSort;
 import com.company.serie1.Algorithms_3_RecursiveFactorial;
+import com.company.serie1.Algorithms_4_SumRecursive;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -13,7 +14,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(Algorithms_3_RecursiveFactorial.recursiveFactorial(4));
+        List<Integer> inputList = new ArrayList<>();
+        inputList.add(1);
+        inputList.add(2);
+        inputList.add(5);
+        inputList.add(8);
+        inputList.add(10);
+
+        Algorithms_4_SumRecursive alg4 = new Algorithms_4_SumRecursive();
+
+        int suma = alg4.sum(inputList);
+        System.out.println(suma);
 
     }
 
---
+++ 3Algorithms/3Algorithms_Exercises/src/com/company/serie1/Algorithms_5_QuickSort.java
@@ -0,0 +1,5 @@
+package com.company.serie1;
+
+public class Algorithms_5_QuickSort {
+
+}
