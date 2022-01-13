6ad2aa37aba716e10eeb958d1eb7d5b710e3ad83
Daniel Kozak <kozak2321@gmail.com>
2022-01-13 16:48:40 +0100

    first stack exercise

--- 1DataStructures/1DataStructures_Exercises/src/com/company/Main.java
+++ 1DataStructures/1DataStructures_Exercises/src/com/company/Main.java
@@ -1,17 +1,13 @@
 package com.company;
 
 import com.company.array.serie_1.Collections_8;
+import com.company.stack.serie_1.Collections_9;
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] firstArray = {1, 3, 4, 5, 7};
-        int[] secondArray = {2, 3, 5, 6};
+        String [] expression = {"10", "2", "8", "*", "+", "3", "-"};
 
-        int[] thirdArray = {2, 5, 6};
-        int[] forthArray = {4, 6, 8, 10};
-
-        Collections_8.unionAndIntersection(firstArray, secondArray);
-        Collections_8.unionAndIntersection(thirdArray, forthArray);
+        Collections_9.evaluatePostfixExpression(expression);
     }
 }
---
+++ 1DataStructures/1DataStructures_Exercises/src/com/company/stack/template/Collections_9.java
@@ -0,0 +1,14 @@
+package com.company.stack.template;
+
+public class Collections_9 {
+
+    /**
+     * Input:
+     * String [] expression = {"10", "2", "8", "*", "+", "3", "-"}
+     * Output : 23
+     */
+
+    public static void evaluatePostfixExpression(String input){
+
+    }
+}
