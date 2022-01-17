dade35febfa55fae4f75979be059bb5482b6329f
Daniel Kozak <kozak2321@gmail.com>
2022-01-17 14:16:18 +0100

    col 13

--- 1DataStructures/1DataStructures_Exercises/src/com/company/Main.java
+++ 1DataStructures/1DataStructures_Exercises/src/com/company/Main.java
@@ -1,38 +1,18 @@
 package com.company;
 
 import com.company.array.serie_1.Collections_8;
-import com.company.stack.serie_1.Collections_10;
-import com.company.stack.serie_1.Collections_11;
-import com.company.stack.serie_1.Collections_9;
-import com.company.stack.serie_1.Collections_12_StackUsingQueue;
+import com.company.stack.serie_1.*;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
+        List<Integer> initList = Arrays.asList(10,20,30,40,50,60,70,80,90,100);
+        Queue<Integer> initQueue = new LinkedList<>(initList);
 
-
-        Collections_12_StackUsingQueue<Integer> a = new Collections_12_StackUsingQueue<>();
-
-        a.push(2);
-        a.print();
-
-
-        a.push(3);
-        a.print();
-
-        a.push(4);
-        a.print();
-        a.pop();
-        System.out.println(a.peek());
-
-//        a.push(4);
-//        a.push(5);
-
-
+        Collections_13 col13 = new Collections_13();
+        col13.reverseFirstKElementsInQueue(initQueue, 4);
 
 
     }
