488fddc77a692fe7b51126d4705adf83a24edc87
Daniel Kozak <kozak2321@gmail.com>
2022-01-18 15:09:39 +0100

    col 19

--- 1DataStructures/1DataStructures_Exercises/src/com/company/Main.java
+++ 1DataStructures/1DataStructures_Exercises/src/com/company/Main.java
@@ -1,6 +1,8 @@
 package com.company;
 
 
+import com.company.graph.serie1.Collections_19;
+import com.company.graph.template.AdjacencyMatrixDirectedRawGraph;
 import com.company.linkedList.serie1.Collections_15;
 import com.company.linkedList.serie1.Collections_16;
 import com.company.linkedList.serie1.Collections_17;
@@ -13,16 +15,32 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        Collections_18 input1 = new Collections_18();
-        input1.insertAtEnd(12);
-        input1.insertAtEnd(11);
-        input1.insertAtEnd(12);
-        input1.insertAtEnd(21);
-        input1.insertAtEnd(41);
-        input1.insertAtEnd(43);
-        input1.insertAtEnd(21);
+        AdjacencyMatrixDirectedRawGraph adjacencyMatrixDirectedRawGraph = new AdjacencyMatrixDirectedRawGraph(7);
+        adjacencyMatrixDirectedRawGraph.addVertex("Pierwszy");
+        adjacencyMatrixDirectedRawGraph.addVertex("Drugi");
+        adjacencyMatrixDirectedRawGraph.addVertex("Trzeci");
+        adjacencyMatrixDirectedRawGraph.addVertex("Czwarty");
+        adjacencyMatrixDirectedRawGraph.addVertex("Piąty");
+        adjacencyMatrixDirectedRawGraph.addVertex("Szósty");
+        adjacencyMatrixDirectedRawGraph.addVertex("Siódmyyyy");
 
-        input1.removeDuplicates();
-        input1.printLinkedList();
+
+
+        adjacencyMatrixDirectedRawGraph.addEdge(0, 1);
+        adjacencyMatrixDirectedRawGraph.addEdge(1, 0);
+        adjacencyMatrixDirectedRawGraph.addEdge(0, 2);
+        adjacencyMatrixDirectedRawGraph.addEdge(2, 0);
+        adjacencyMatrixDirectedRawGraph.addEdge(2, 3);
+        adjacencyMatrixDirectedRawGraph.addEdge(3, 2);
+        adjacencyMatrixDirectedRawGraph.addEdge(2, 4);
+        adjacencyMatrixDirectedRawGraph.addEdge(4, 2);
+        adjacencyMatrixDirectedRawGraph.addEdge(3, 5);
+        adjacencyMatrixDirectedRawGraph.addEdge(5, 3);
+        adjacencyMatrixDirectedRawGraph.addEdge(1, 6);
+        adjacencyMatrixDirectedRawGraph.addEdge(6, 1);
+
+
+        Collections_19.breadthFirstSearch(adjacencyMatrixDirectedRawGraph);
+
     }
 }
