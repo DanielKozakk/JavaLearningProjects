ebe39f31f2e4e36787f0a84ffbc0a66cfa048cde
Daniel Kozak <kozak2321@gmail.com>
2022-01-19 15:59:49 +0100

    col 23

--- 1DataStructures/1DataStructures_Exercises/src/com/company/Main.java
+++ 1DataStructures/1DataStructures_Exercises/src/com/company/Main.java
@@ -1,44 +1,52 @@
 package com.company;
 
 
-import com.company.graph.serie1.Collections_22;
 import com.company.graph.template.AdjacencyListDirectedRawGraph;
-import com.company.graph.serie1.Collections_21;
+import com.company.graph.serie1.Collections_23;
 
 public class Main {
 
     public static void main(String[] args) {
-        AdjacencyListDirectedRawGraph adjacencyMatrixDirectedRawGraph = new AdjacencyListDirectedRawGraph(7);
-        adjacencyMatrixDirectedRawGraph.addVertex("ZERO");
-        adjacencyMatrixDirectedRawGraph.addVertex("JEDEN");
-        adjacencyMatrixDirectedRawGraph.addVertex("DWA");
-        adjacencyMatrixDirectedRawGraph.addVertex("TRZY");
-        adjacencyMatrixDirectedRawGraph.addVertex("CZTERY");
-        adjacencyMatrixDirectedRawGraph.addVertex("PIĘĆ");
-        adjacencyMatrixDirectedRawGraph.addVertex("SZEŚĆ");
+        AdjacencyListDirectedRawGraph col23 = new AdjacencyListDirectedRawGraph(8);
+        col23.addVertex("Zerowy");
+        col23.addVertex("Pierwszy");
+        col23.addVertex("Drugi");
+        col23.addVertex("Trzeci");
+        col23.addVertex("Czwarty");
+        col23.addVertex("Piąty");
+        col23.addVertex("Szósty");
+        col23.addVertex("Siódmy");
 
 
+        col23.addEdge(0, 1);
+        col23.addEdge(1, 0);
 
-        adjacencyMatrixDirectedRawGraph.addEdge(0, 1);
-        adjacencyMatrixDirectedRawGraph.addEdge(1, 0);
+        col23.addEdge(0, 2);
+        col23.addEdge(2, 0);
 
-        adjacencyMatrixDirectedRawGraph.addEdge(0, 2);
-        adjacencyMatrixDirectedRawGraph.addEdge(2, 0);
+        col23.addEdge(1, 2);
+        col23.addEdge(2, 1);
 
-        adjacencyMatrixDirectedRawGraph.addEdge(2, 3);
-        adjacencyMatrixDirectedRawGraph.addEdge(3, 2);
+        col23.addEdge(3, 2);
+        col23.addEdge(2, 3);
 
-        adjacencyMatrixDirectedRawGraph.addEdge(2, 4);
-        adjacencyMatrixDirectedRawGraph.addEdge(4, 2);
+        col23.addEdge(4, 2);
+        col23.addEdge(2, 4);
 
-        adjacencyMatrixDirectedRawGraph.addEdge(3, 5);
-        adjacencyMatrixDirectedRawGraph.addEdge(5, 3);
+        col23.addEdge(3, 5);
+        col23.addEdge(5, 3);
 
-        adjacencyMatrixDirectedRawGraph.addEdge(1, 6);
-        adjacencyMatrixDirectedRawGraph.addEdge(6, 1);
+        col23.addEdge(1, 7);
+        col23.addEdge(7, 1);
 
+        col23.addEdge(1, 6);
+        col23.addEdge(6, 1);
 
-        Collections_22.depthFirst(adjacencyMatrixDirectedRawGraph);
+        col23.addEdge(7, 6);
+        col23.addEdge(6, 7);
+
+        System.out.println(Collections_23.checkIfGraphIsTree(col23));
+
 
     }
 }
---
+++ 1DataStructures/1DataStructures_Exercises/src/com/company/graph/template/Collections_23.java
@@ -0,0 +1,43 @@
+package com.company.graph.template;
+
+public class Collections_23  {
+
+    /**
+     AdjacencyListDirectedRawGraph col23 = new AdjacencyListDirectedRawGraph(7);
+     col23.addVertex("Zerowy");
+     col23.addVertex("Pierwszy");
+     col23.addVertex("Drugi");
+     col23.addVertex("Trzeci");
+     col23.addVertex("Czwarty");
+     col23.addVertex("Piąty");
+     col23.addVertex("Szósty");
+
+     col23.addEdge(0, 1);
+     col23.addEdge(1, 0);
+
+     col23.addEdge(0, 2);
+     col23.addEdge(2, 0);
+
+
+     col23.addEdge(1,6);
+     col23.addEdge(6,1);
+
+     col23.addEdge(2,3);
+     col23.addEdge(3,2);
+
+     col23.addEdge(2,4);
+     col23.addEdge(4,2);
+
+     col23.addEdge(5,3);
+     col23.addEdge(3,5);
+
+
+
+
+     * @param input
+     */
+    public static void checkIfGraphIsTree(AdjacencyListDirectedRawGraph input){
+
+
+    }
+}
