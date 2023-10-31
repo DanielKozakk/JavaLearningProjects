56ffb55b2de73116cb35a7b72a705fd3d0fb60d0
Daniel Kozak <kozak2321@gmail.com>
2023-10-31 13:46:39 +0100

    fix

--- HeadFirstExperiment/src/Main.java
+++ HeadFirstExperiment/src/Main.java
@@ -1,13 +1,10 @@
 import chapter16_SerializationAndWritingToFiles.flashcards.QuizCardBuilder;
 
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class Main<T> {
 
-    public static void main(String[] args) {
-
-        new QuizCardBuilder().go();
+    public static void main(String[] args) throws IOException {
 
     }
 }
